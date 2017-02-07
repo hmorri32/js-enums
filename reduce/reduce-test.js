4e723cad860cbc9c15f1a46e410f64194fd74235
Hugh Morrison <hmorri32@gmail.com>
2017-02-07 09:58:03 -0700

    first commit

--- reduce/reduce-test.js
+++ reduce/reduce-test.js
@@ -2,11 +2,15 @@ require('mocha')
 require('locus')
 const expect = require('chai').expect
 
-describe('map tests', () => {
+describe('reduce tests', () => {
 
-  it('should add all the numbes together', () => {
+  it('should add all the numbers together', () => {
     let numbers = [32, 1, 21, 5, 81, 333]
+
     // your code goes here
+    let result = numbers.reduce((a, b)=>{
+      return a + b
+    }, 0)
 
     expect(result).to.equal(473)
   })
@@ -14,17 +18,33 @@ describe('map tests', () => {
   it('should subtract all the numbers', () => {
     let numbers = [28, 12, 38, 1, 91]
 
+    let results = numbers.reduce((a, b)=>{
+      return a - b
+    }, 0)
+
     expect(results).to.equal(-170)
   })
 
   it('should multiply a list of number', ()=> {
     let numbers = [2, 3, 5, 7]
-      //initial value is 1
-      //Your code goes here
+
+    let results = numbers.reduce((a, b)=>{
+      return a * b
+    }, 1)
+
     expect(results).to.equal(210)
   })
-  it.skip('subtrack the smallest number' , () => {
+
+  it('subtract the smallest number' , () => {
     let elements = [[8, 5, 3], [1, 9, 11], [4, 7, 2], [19, 34, 6]]
+
+    let stuff = elements.map((array)=>{
+      return Math.min(array)
+    })
+    let results = stuff.reduce((stuff)=>{
+      return stuff
+    }, 100)
+
     // initial value is going to be 100
     // your code goes here
     expect(results).to.equal(88)
@@ -37,7 +57,7 @@ describe('map tests', () => {
     expect(results).to.equal(31);
   });
 
-  it('divide 560 by a gang of numbers ', () => {
+  it.skip('divide 560 by a gang of numbers ', () => {
     let numbers = [2, 2, 2, 5, 7]
      // inital value is 560
      // your code goes here
@@ -45,14 +65,14 @@ describe('map tests', () => {
     expect(results).equal(2);
   })
 
-  it.only('capitalize keywords in a phrase', () => {
+  it.skip('capitalize keywords in a phrase', () => {
     let keywords = ["fish", "blue"]
     let saying = 'one fish two fish red fish blue fish'
      // your code goes here
     expect(results).to.equal('one FISH two FISH red FISH BLUE FISH')
   })
 
-  it('find smallest key and value inside of the object', () => {
+  it.skip('find smallest key and value inside of the object', () => {
     let arrayOfObjects = {a: 100, b: 200, c: 300, d: 600}
     // your code goes here
 
