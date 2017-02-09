92b9e93d9626605789112e8ad3abfbe384501d92
Hugh Morrison <hmorri32@gmail.com>
2017-02-09 14:20:34 -0700

    fix filter test

--- filter/filter-test.js
+++ filter/filter-test.js
@@ -1,62 +1,77 @@
-require('mocha')
-require('locus')
+
 const expect = require('chai').expect
 
-describe('map tests', () => {
+describe('filter tests', () => {
 
-  it('should add all the numbes together', () => {
-    let numbers = [32, 1, 21, 5, 81, 333]
+  it('should pick the even numbers ', () => {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     // your code goes here
-
-    expect(result).to.equal(473)
-  })
-
-  it('should subtract all the numbers', () => {
-    let numbers = [28, 12, 38, 1, 91]
-
-    expect(results).to.equal(-170)
-  })
-
-  it('should multiply a list of number', ()=> {
-    let numbers = [2, 3, 5, 7]
-      //initial value is 1
-      //Your code goes here
-    expect(results).to.equal(210)
-  })
-  it.skip('subtrack the smallest number' , () => {
-    let elements = [[8, 5, 3], [1, 9, 11], [4, 7, 2], [19, 34, 6]]
-    // initial value is going to be 100
-    // your code goes here
-    expect(results).to.equal(88)
-  })
-
-  it.skip('all second values add together', () => {
-    let elements = [["a", 1], ["b", 9], ["c", 21]]
-    // initial value is 10
-    // your code goes here
-    expect(results).to.equal(31);
+    expect(results).to.deep.equal([2, 4, 6, 8, 10])
   });
 
-  it('divide 560 by a gang of numbers ', () => {
-    let numbers = [2, 2, 2, 5, 7]
-     // inital value is 560
-     // your code goes here
+  it('should pick the odd numbers ', () => {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    expect(results).equal(2);
-  })
-
-  it.only('capitalize keywords in a phrase', () => {
-    let keywords = ["fish", "blue"]
-    let saying = 'one fish two fish red fish blue fish'
-     // your code goes here
-    expect(results).to.equal('one FISH two FISH red FISH BLUE FISH')
-  })
-
-  it('find smallest key and value inside of the object', () => {
-    let arrayOfObjects = {a: 100, b: 200, c: 300, d: 600}
     // your code goes here
 
-    expect(smallestValue).to.deep.equal({a:100})
-  })
+    expect(results).to.deep.equal([1, 3, 5, 7, 9])
+  });
+
+  it('should pick wordfs with three letteres ', () => {
+    let words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
+
+    // your code goes here
+
+    expect(results).to.deep.equal(["bad", "cat", "dog", "red"])
+  });
+
+  it('should pick arrays only', () => {
+    let elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
+
+    // your code goes here
+
+    expect(results).to.deep.equal([["dog"], [56, 3, 8]])
+  });
+
+  it('should remove specific array from object', () => {
+    let human = {name: 'yung-jhun', songs:[['cool song 1', 50], ['cool song 2', 100],['cool song 3', 90]]}
+    let tracker = {person: human}
+    let saying = 'cool song 2'
+
+    // your code goes here.
+
+    expect(human.songs).to.deep.equal([['cool song 1', 50],['cool song 3', 90]])
+  });
+
+  it('should filter based off of strings from user ', () => {
+    let userString = 'su'
+    let messages = [{user:'louisa', message: 'haaaaa'},
+                    {user:'yung-jhun', message: 'kewl dude'},
+                    {user: 'alter-nate', message:'suh kewl'},
+                    {user:'meeka', message: 'adfasuhlkj'},
+                    {user: 'robbie', message: 'im super loud'}]
+
+    // your code goes here
+
+    expect(results).to.deep.equal([{user: 'alter-nate', message:'suh kewl'},
+                                   {user:'meeka', message: 'adfasuhlkj'},
+                                   {user: 'robbie', message: 'im super loud'}])
+  });
+
+  it('should return the correct ids', ()=> {
+    let data = [{ account: 'person 1', id: '00112'},
+                { account: 'person 2', id: '00045'},
+                { account: 'person 3', id: '12309'},
+                { account: 'person 4', id: '55931'},
+                { account: 'person 5',id: '90215'}];
+
+    let ids = ['00112', '90215', '12309']
+
+    // your code goes here
+
+    expect(results).to.deep.equal([{ account: 'person 1', id: '00112'},
+                                   { account: 'person 3', id: '12309'},
+                                   { account: 'person 5', id: '90215'}])
+  });
 
 });
